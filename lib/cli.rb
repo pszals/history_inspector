3f4a49d4a0853640dd3d720a6b82bd58037d8815
Philip Szalwinski <pszalwinski@gmail.com>
2016-10-25 15:30:19 +0100

    Add bar graph to output

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,31 @@
 require './lib/inspector'
 
 class CLI
-  attr_reader :std_in, :std_out
+  attr_reader :std_in, :std_out, :inspector
 
   def initialize(std_in = STDIN, std_out = STDOUT)
     @std_in = std_in
     @std_out = std_out
+    @inspector ||= Inspector.new
   end
 
   def print_top_ten
-    std_out.puts format_results(Inspector.new.top_ten)
+    std_out.puts format_results(inspector.top_ten)
+    std_out.puts bar_graph(inspector.top_ten)
+  end
+
+  def bar_graph(stats)
+    out_of_100(stats).reduce("") do |memo, (command, occurences)|
+      memo += "#{'#'*occurences} #{occurences.to_s}% -- #{command.to_s}\n"
+    end
+  end
+
+  def out_of_100(stats)
+    total = stats.reduce(0) {|memo, (command, occurences)| memo += occurences}
+    stats.map { |command, occurences| [command, occurences = (occurences.to_f/total.to_f*100).round] }
   end
 
   def format_results(results)
-    results.map {|command, count| "#{count} #{command}"}
+    results.map { |command, count| "#{count} #{command}" }
   end
 end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -17,15 +17,34 @@ describe CLI do
   let(:spy) { Spy.new }
   let(:cli) { CLI.new(double, spy) }
 
-  it 'prints the top ten results' do
+  it 'prints the top ten results with a graph' do
     allow_any_instance_of(Inspector).to receive(:top_ten).and_return(fake_results)
 
     cli.print_top_ten
 
     expect(spy.output.first).to eq(formatted_results)
+    expect(spy.output.last).to include(cli.bar_graph(fake_results))
   end
 
   it 'formats results' do
     expect(cli.format_results(fake_results)).to eq(formatted_results)
+  end
+
+  it 'displays bars as a representation of percentage of commands' do
+    foo_count = 67
+    bar_count = 33
+    stats = [["foo", foo_count], ["bar", bar_count ]]
+    foo_bars = "#"*foo_count
+    bar_bars = "#"*bar_count
+
+    expect(cli.bar_graph(stats)).to include("#{foo_bars} #{foo_count}")
+  end
+
+  it 'converts counts to a number out of 100' do
+    foo_count = 90
+    bar_count = 30
+    stats = [["foo", foo_count], ["bar", bar_count ]]
+
+    expect(cli.out_of_100(stats)).to eq([["foo", 75], ["bar", 25]])
   end
 end
